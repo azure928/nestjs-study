2a68f9de22dddf046e720794717dcc6e445148c0
azure928 <azure6.dev@gmail.com>
2023-04-21 23:59:40 +0900

    chore: http-exception.filter.ts 폴더 변경

--- src/exception/exception.module.ts
+++ src/exception/exception.module.ts
@@ -1,6 +1,6 @@
 import { Logger, Module } from '@nestjs/common';
 import { APP_FILTER } from '@nestjs/core';
-import { HttpExceptionFilter } from '../common/filters/http-exception.filter';
+import { HttpExceptionFilter } from './http-exception.filter';
 
 @Module({
   providers: [Logger, { provide: APP_FILTER, useClass: HttpExceptionFilter }],
