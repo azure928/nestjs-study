d9b19c1e6986cae4e1114a6d98f785d037f4bbb2
azure928 <azure6.dev@gmail.com>
2023-04-21 14:50:13 +0900

    add: create cats service

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsController } from './cats/cats.controller';
 import { LoggerMiddleware } from './common/middleware/logger.middleware';
+import { CatsService } from './cats/cats.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController, CatsController],
-  providers: [AppService],
+  controllers: [CatsController],
+  providers: [CatsService],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
