b73ad1261158b9a73dbc7451d918e2f35533bebc
azure928 <azure6.dev@gmail.com>
2023-04-29 03:17:08 +0900

    add: auth 모듈 service의 로그인 처리 메서드에서 jwt 토큰을 반환하도록 구현

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { LoggerMiddleware } from './common/middleware/logger.middleware';
 import { CatsModule } from './cats/cats.module';
 import { ExceptionModule } from './exception/exception.module';
 import { LoggingModule } from './logging/logging.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [CatsModule, ExceptionModule, LoggingModule],
+  imports: [CatsModule, ExceptionModule, LoggingModule, AuthModule, UsersModule],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
